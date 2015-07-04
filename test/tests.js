0b5514865f0ac38ba5913d5ebbf17f5460573c15
Ben <benfahrenheit@users.noreply.github.com>
2015-07-03 22:50:14 -0400

    Messing around with some sample tests

--- test/tests.js
+++ test/tests.js
@@ -1,2 +1,13 @@
 /// <reference path="../typings/mocha/mocha.d.ts"/>
 var assert = require('assert');
+
+// Yeah, I know this doesn't have anything to do with this code.
+// Just playing around with some samples :)
+
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			assert.equal(-1, [1,2,3].indexOf(5));
+		});
+	});
+});
